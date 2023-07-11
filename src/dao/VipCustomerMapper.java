52b643e9f96799bf2627a05ea0f27dbddb35b2df
LiangJie <1134772893@qq.com>
2023-07-11 16:59:05 +0800

    修改test，测试通过VipCustomerService mapper修正了一些错误

--- src/dao/VipCustomerMapper.java
+++ src/dao/VipCustomerMapper.java
@@ -4,8 +4,8 @@ import entity.VipCustomer;
 import org.apache.ibatis.annotations.Insert;
 
 public interface VipCustomerMapper {
-    @Insert("INSERT INTO vipcustomer (companyName,id,regTime,password,contractAmount,recharge,contactPerson,employee) "+
-            " VALUES (#{companyName},#{id},#{regTime},#{password},#{contractAmount},#{recharge},#{contactPerson},#{employee})")
+    @Insert("INSERT INTO vipcustomer (productType,companyName,website,id,regTime,password,contractAmount,recharge,contactPerson,employee) "+
+            " VALUES (#{productType},#{companyName},#{website},#{id},#{regTime},#{password},#{contractAmount},#{recharge},#{contactPerson},#{employee})")
     public int addVipCus(VipCustomer vipCustomer);
 
 }
--- src/test/VipTest.java
+++ src/test/VipTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import dao.VipCustomerMapper;
 import entity.VipCustomer;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -8,6 +7,7 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
+import service.VipCustomerService;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,18 +23,19 @@ public class VipTest {
     @Test
     public void setVipCustomerTest(){
         VipCustomer vipCustomer = new VipCustomer();
+        vipCustomer.setProductType("type");
         vipCustomer.setCompanyName("testCompany");
+        vipCustomer.setWebsite("webt");
         vipCustomer.setContractAmount(111111);
         vipCustomer.setContactPerson("tesstperson");
         vipCustomer.setEmployee("testemp");
         vipCustomer.setRecharge("re?");
         vipCustomer.setRegTime("reg1111");
-        vipCustomer.setSystemId(12351135);
         vipCustomer.setId(1);
         vipCustomer.setSystemId(1);
         vipCustomer.setPassword("121gaoi");
-        VipCustomerMapper vipCustomerMapper = session.getMapper(VipCustomerMapper.class);
-        int i = vipCustomerMapper.addVipCus(vipCustomer);
+        VipCustomerService vipCustomerService = new VipCustomerService();
+        int i = vipCustomerService.addVipCus(vipCustomer);
         System.out.print(i);
 
     }
